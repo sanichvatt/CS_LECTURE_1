04dda2edffa9352012f542437edd5c4c41bfb30d
sanich <sanich.vatt@mail.ru>
2023-06-21 02:21:50 +0300

    Home work TASK 1

---
+++ HOME WORK_1/TASK_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход 
+// принимает два числа и выдает, какое число большее, 
+// а какое меньшее.
+
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (num1 > num2) 
+{
+    max = num1;    
+}
+else
+{
+    max = num2;
+}
+Console.Write($"Наибольше число -> {max}");
