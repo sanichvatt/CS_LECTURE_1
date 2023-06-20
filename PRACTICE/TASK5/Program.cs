d04f4d3b9349cfbcd80f478e370707dc06d1706b
sanich <sanich.vatt@mail.ru>
2023-06-21 01:24:22 +0300

    TASK 5

---
+++ PRACTICE/TASK5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num;
+if (num < 0)
+{
+    res = -num;
+}
+
+Console.WriteLine($"Модуль числа равен {res}");
