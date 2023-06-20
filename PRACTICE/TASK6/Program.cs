f59292385066f11d487ea45f02c1a4bb8f502b32
sanich <sanich.vatt@mail.ru>
2023-06-21 01:44:27 +0300

    TASK 6

---
+++ PRACTICE/TASK6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке (-N;+N)
+
+Console.Write("введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+while (i <= num) 
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+
