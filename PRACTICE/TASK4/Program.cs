cca1e1cad92e35b3951aa02ce9a6d0d17885be88
sanich <sanich.vatt@mail.ru>
2023-06-21 01:20:31 +0300

    TASK 4

---
+++ PRACTICE/TASK4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу вычисления функции:
+// x = f(a)
+
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int numInt = Convert.ToInt32(num);
+Console.WriteLine(numInt * numInt * numInt);
+
