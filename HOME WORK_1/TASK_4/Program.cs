c41816c5de1d79107e17d23c0f65946c09f1fe66
sanich <sanich.vatt@mail.ru>
2023-06-22 01:30:33 +0300

    Home work 1_TASK4

---
+++ HOME WORK_1/TASK_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа в данном интервале: ");
+int i =  1;
+int div = 1;
+while (i <= num)
+{
+    if (div == 0)
+    {
+        Console.Write($"{i}, ");
+    }
+    i++;
+    div = i % 2;
+
+}
