9d4841b565b4795a0b584559e6bf9979a5f89972
sanich <sanich.vatt@mail.ru>
2023-06-22 21:56:20 +0300

    LECTURE2_Examples010 Method Array

---
+++ LECTURE2/Examples010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int[] array = { 1, 12, 18, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
