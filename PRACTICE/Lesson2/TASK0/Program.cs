37adde41807a8413a17513723f9d3bb6f7a6acd8
sanich <sanich.vatt@mail.ru>
2023-06-23 00:55:46 +0300

    PRACTICE Lesson2 TASK0

---
+++ PRACTICE/Lesson2/TASK0/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на
+// выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+//Функция вводит целое значение из консоли
+
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool Validate3sing(int number)
+{
+    if(number < 100 || number >= 1000)
+    {
+        Console.WriteLine("Вы ввели не 3-х значное число");
+        return false;
+    }
+    return true;
+}
+
+int digit = ReadInt("Введите 3-х значное число -> ");
+if (Validate3sing(digit))
+{
+    int LastDigit = digit % 10;
+    System.Console.WriteLine($"Третья цифра числа {digit} равна {LastDigit}");
+}
