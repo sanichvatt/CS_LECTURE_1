7838930d2f45a05a3673366c50c91dd22d40f7d6
sanich <sanich.vatt@mail.ru>
2023-06-22 23:07:21 +0300

    LECTURE2_Examples011 Array Library

---
+++ LECTURE2/Examples011_ArrayLibrary/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOff(array, 444);
+Console.WriteLine($"Позиция = {pos}");
