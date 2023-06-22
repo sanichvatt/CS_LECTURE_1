e4c8d385848744e6ae9d62b07d6f72cf0457aa32
sanich <sanich.vatt@mail.ru>
2023-06-23 01:46:27 +0300

    PRACTICE Lesson2 TASK1

---
+++ PRACTICE/Lesson2/TASK1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка (10, 99) и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+System.Console.Clear();
+int runNum = new Random().Next(10, 100);
+
+int Max(int Num)
+{
+    int num1 = runNum % 10;
+    int num2 = runNum / 10 % 10;
+    if (num1 > num2) return num1;
+    else return num2;
+}
+
+System.Console.Write($"{runNum} -> ");
+System.Console.Write(Max(runNum));
