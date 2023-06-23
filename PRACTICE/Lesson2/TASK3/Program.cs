1acf46f6160fa99f43003e995a60f569bf3468a5
sanich <sanich.vatt@mail.ru>
2023-06-24 00:01:31 +0300

    PRACTICE Lesson2 TASK3

---
+++ PRACTICE/Lesson2/TASK3/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear();
+
+// Напишите программу которая принимает на вход число и проверяет кратно ли оно оновременно 7 и 23
+
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int ResT (int arg1, int arg2)
+{
+    int result = arg1 % arg2;
+    return result;
+}
+
+bool AliQuot (int arg1, int arg2, int arg3)
+{
+    return arg1 % arg2 == 0 && arg1 % arg3 == 0;
+    // if (arg1 % arg2 == 0 && arg1 % arg3 == 0)
+    // {
+    //     return true;
+    // }
+    // else { return false; }
+}
+
+int num = ReadInt("Введите число -> ");
+int a = 7;
+int b = 23;
+
+if (AliQuot(num, a, b) == true)
+{
+    Console.Write($"{num} кратно {a} и {b}");
+}
+else
+{
+    Console.Write($"{num} не кратно {a} и {b}");
+}
+
+// int rest1 = ResT(num, a);
+// int rest2 = ResT(num, b);
+// if (rest1 == 0 || rest2 == 0)
+// {
+//     Console.Write($"{num} кратно {a} и {b}");
+// }
+// else 
+// {
+//     Console.Write($"{num} не кратно {a} и {b}");
+// }
