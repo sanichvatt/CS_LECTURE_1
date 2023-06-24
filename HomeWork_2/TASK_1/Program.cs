86e70bfada28d374b670dcc16b736f4ff70e7f9c
sanich <sanich.vatt@mail.ru>
2023-06-25 01:35:39 +0300

    HomeWork 2 TASK1

---
+++ HomeWork_2/TASK_1/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.Clear();
+// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int WriteInt(string messege)
+{
+    System.Console.Write(messege);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int num = WriteInt("Введите трёхзначное число -> ");
+
+if (num < 99 || num > 999)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    int b = num % 100 / 10;
+    Console.WriteLine($"{num} -> {b}");
+}
