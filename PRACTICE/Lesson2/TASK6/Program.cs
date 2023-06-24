9e5105194e81268a54a0f4a90e4b4ea1217546a5
sanich <sanich.vatt@mail.ru>
2023-06-25 00:30:20 +0300

    PRACTICE Lesson2 TASK6

---
+++ PRACTICE/Lesson2/TASK6/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    int namber = Convert.ToInt32(Console.ReadLine());
+    return namber;
+}
+int num = ReadInt("Введите число для проверки кратности цикла -> ");
+
+void Fn(int num)
+{
+    Random rand = new Random();
+    int digit;
+    do
+    {
+        digit = rand.Next(0, 1000);
+        System.Console.WriteLine(digit);
+    }
+    while(digit % num != 0);
+}
+Fn(num);
