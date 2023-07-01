dd3d92ceeb7016faf6b10b276a8b4c88d194712f
sanich <sanich.vatt@mail.ru>
2023-07-01 23:38:54 +0300

    PRACTICE Lesson3 TASK3

---
+++ PRACTICE/Lesson3/TASK3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int Prompt(string message)
+{
+    Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine());
+}
+int number = Prompt("Введите число: ");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+}
