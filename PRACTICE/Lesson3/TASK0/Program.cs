c42808836a192b1ab8872f8df01642a389f23edb
sanich <sanich.vatt@mail.ru>
2023-07-01 03:50:19 +0300

    PRACTICE Lesson3 TASK0

---
+++ PRACTICE/Lesson3/TASK0/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Clear();
+
+static int wwwwwwwww(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        return 1;
+    }
+    if (x < 0 && y > 0)
+    {
+        return 2;
+    }
+    if (x < 0 && y < 0)
+    {
+        return 3;
+    }
+    return 4;
+}
+
+int Prompt(string messege)
+{
+    Console.Write($"{messege} > ");
+    return int.Parse(Console.ReadLine());
+}
+bool ValidateCoords(int x1, int y1)
+{
+    if (x1 == 0 || y1 == 0)
+    {
+        Console.WriteLine("Одна из координат равна 0");
+        return false;        
+    }
+    return true;
+}
+
+int x = Prompt("Введите X");
+int y = Prompt("Введите Y");
+if (ValidateCoords(x, y))
+{
+    int quarter = wwwwwwwww(x, y);
+    System.Console.WriteLine($"Четверть номер {quarter}");
+}
