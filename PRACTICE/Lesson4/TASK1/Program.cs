d1fc499d5a53ddf0b7da47f4776cfac763af1e90
sanich <sanich.vatt@mail.ru>
2023-07-02 02:26:25 +0300

    PRACTICE Lesson4 TASK1

---
+++ PRACTICE/Lesson4/TASK1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А)
+// и выдает сумму чисел от 1 до А.
+
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не число.");
+    Environment.Exit(0);
+    return 0;
+
+    // throw new Exception("Вы ввели не число.");
+}
+
+bool ValidatePositiveNumber(int number)
+{
+    if (number < 0)
+    {
+        System.Console.Write("Введенное число не положительное");
+        return false;
+    }
+    return true;
+}
+
+int SumFrom0ToNumber(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+
+int number = ReadInt("Введите целое число");
+if (ValidatePositiveNumber(number))
+{
+    Console.WriteLine($"Сумма чисел от 1 до {number} равна -> {SumFrom0ToNumber(number)}");
+}
