8909d0fc7287d8208d1b14b0811e2e86bbf2f04e
sanich <sanich.vatt@mail.ru>
2023-07-01 06:12:52 +0300

    HomeWork_3 TASK1

---
+++ HomeWork_3/TASK1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+int Prompt(string messege)
+{
+    Console.Write($"{messege}");
+    return int.Parse(Console.ReadLine());
+}
+
+bool If5num(int arg1)
+{
+    if (arg1 > 9999 && arg1 < 100000) return true;        
+    return false;
+}
+
+int num = Prompt("Введите пятизначное число: ");
+int a1 = num / 10000 % 10;
+int a2 = num / 1000 % 10;
+int a3 = num / 10 % 10;
+int a4 = num / 1 % 10;
+
+if (If5num(num)) 
+{
+    if (a1 == a4 && a2 == a3)
+    {
+        Console.WriteLine($"{num} -> ДА");
+    }
+    else Console.WriteLine($"{num} -> НЕТ");
+}
+else Console.WriteLine("Число не пятизначное");
+
