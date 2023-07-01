375291f2f3a8f512513a40b83cc8b58bcfae5ad2
sanich <sanich.vatt@mail.ru>
2023-07-01 05:07:19 +0300

    PRACTICE Lesson3 TASK2

---
+++ PRACTICE/Lesson3/TASK2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int Prompt(string messege)
+{
+    Console.Write($"{messege}");
+    return int.Parse(Console.ReadLine());
+}
+
+int x1 = Prompt("Введите X1: ");
+int y1 = Prompt("Введите Y1: ");
+int x2 = Prompt("Введите X2: ");
+int y2 = Prompt("Введите Y2: ");
+
+double dx = x2 - x1;
+double dy = y2 - y1;
+double distance = Math.Sqrt(dx * dx + dy * dy);
+Console.WriteLine($"Расстояние между точками равно {distance}");
