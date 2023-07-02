ffd1876e263b31b6d282816e18da39a49cbe8478
sanich <sanich.vatt@mail.ru>
2023-07-02 13:07:40 +0300

    HomeWork_4 TASK1

---
+++ HomeWork_4/TASK1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int ReadInt(string message)
+// {
+//     Console.Write($"{message}");
+//     string inputedString = Console.ReadLine();
+//     int arg = Convert.ToInt32(inputedString);
+//     return arg;
+// }
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write($"{message}");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+    System.Console.WriteLine("Вы ввели не число.");
+    Environment.Exit(0);
+    return 0;
+
+    // throw new Exception("Вы ввели не число.");
+}
+
+bool ValidatePositiveNumber(int number1, int number2)
+{
+    if (number1 < 0 || number2 < 0)
+    {
+        System.Console.Write("Введенное число не положительное");
+        return false;
+    }
+    return true;
+}
+
+int num1 = ReadInt("Введите целое число A -> ");
+int num2 = ReadInt("Введите степень целого числа B -> ");
+double pow = Math.Pow(num1, num2);
+if (ValidatePositiveNumber(num1, num2))
+{
+    Console.WriteLine($"Степень числа {num1} равна -> {pow}");
+}
