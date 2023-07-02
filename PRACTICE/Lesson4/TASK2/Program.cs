5d1c27e3bed8b782f4ab35d991bfa5d00dcedea4
sanich <sanich.vatt@mail.ru>
2023-07-02 03:09:46 +0300

    PRACTICE Lesson4 TASK2

---
+++ PRACTICE/Lesson4/TASK2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = ReadInt("Введите число -> ");
+
+int count = CountDigits(number);
+System.Console.WriteLine($"В числе {number} - {count} цифр");
+
+static int CountDigits(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number /= 10;
+    }
+
+    return count;
+}
