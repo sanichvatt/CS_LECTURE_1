68ce27911822e6fea895e4205049d58fcaf1c2af
sanich <sanich.vatt@mail.ru>
2023-07-02 13:42:11 +0300

    HomeWork_4 TASK3

---
+++ HomeWork_4/TASK3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int[] IntArray(int length)
+{
+    int[] arry = new int[8];
+    for (int i = 0; i < arry.Length; i++)
+    {
+        arry[i] = Convert.ToInt32(Console.ReadLine());        
+    }
+    return arry;
+}
+
+
+Console.WriteLine ($"Введите массив чисел");
+int[] arr8 = IntArray(8);
+for (int i = 0; i < arr8.Length; i++)
+{
+    Console.Write($"{arr8[i]}, ");
+}
