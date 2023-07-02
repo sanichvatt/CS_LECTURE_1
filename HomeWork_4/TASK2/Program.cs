d23705316cc9d316eba49153c05166ba4260b078
sanich <sanich.vatt@mail.ru>
2023-07-02 12:50:06 +0300

    HomeWork_4 TASK2 it 1

--- HomeWork_4/TASK2/Program.cs
+++ HomeWork_4/TASK2/Program.cs
@@ -39,31 +39,52 @@ int CountDigits(int number)
 
 int numlength = CountDigits(num);
 
-int[] CountArray(int length)
+// int[] CountArray(int length)
+// {
+//     int[] arry = new int[numlength];
+//     for (int i = 0; i < arry.Length; i++)
+//     {
+//         arry[i] = num / 10 % 10;
+//         num /=10;
+//     }
+//     return arry;
+// }
+
+
+// int CalculateValue(int[] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         sum = array[i] + array[i+1];        
+//     }
+//     return sum;
+// }
+
+
+// double sum = 0;
+//     for (int i = 1; i < numlength; i++)
+//     {
+//         sum = (num / (Math.Pow(10, i)) % 10) + (num / (Math.Pow(10, (i+1))) % 10);      
+//     }
+
+int sum = 0;
+double arg1 = 0;
+double arg2 = 0;
+int arg3 = 0;
+int arg4 = 0;
+for (int i = 0; i < numlength; i++)
 {
-    int[] arry = new int[numlength];
-    for (int i = 0; i < arry.Length; i++)
-    {
-        arry[i] = num / 10 % 10;
-        num /=10;
-    }
-    return arry;
+    arg1 = Math.Pow(10, (i + 1));
+    arg3 = Convert.ToInt32(arg1);
+    arg2 = Math.Pow(10, i);
+    arg4 = Convert.ToInt32(arg2);
+    sum = sum + num % arg3 / arg4;
 }
 
-
-int CalculateValue(int[] array)
-{
-    int sum = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        sum = array[i] + array[i+1];        
-    }
-    return sum;
-}
-
-
-int[] arrnum = CountArray(num);
+// dob = num % 100 / (Math.Pow(10, 1));
+// int sum = Convert.ToInt32(dob);
 if (ValidatePositiveNumber(num))
 {
-    Console.WriteLine($"Сумма цифр числа {num} равна -> {CalculateValue(arrnum)}");
+    Console.WriteLine($"Сумма цифр числа {num} равна -> {sum}");
 }
