d0325682eafb59172e80c8b02403a3a1ab03a180
sanich <sanich.vatt@mail.ru>
2023-07-06 20:49:58 +0300

    LECTURE3 Examples 012_Methods

---
+++ LECTURE3/Examples012_Metods/Program.cs
@@ -0,0 +1,56 @@
+﻿// Вид 1
+Console.Clear();
+// void Method1()
+// {
+//     System.Console.WriteLine("Автор Угольцов Александр");
+// }
+
+// Method1();
+
+// Вид 2
+
+// void Method2(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+
+// Method2("Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while(i<count)
+//     {
+//         System.Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+
+// Method21(msg:"Текст", count: 4);
+// Method21(count: 4, msg:"Текст");
+
+// Вид 3
+
+// int Method3();
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// System.Console.WriteLine(year);
+
+// Вид 4
+
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "Z");
+System.Console.WriteLine(res);
