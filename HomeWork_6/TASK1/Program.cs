97948fb2abf653ecc269220a5a29d16db3c755cb
sanich <sanich.vatt@mail.ru>
2023-07-08 23:35:01 +0300

    HomeWork 6 TASK1

---
+++ HomeWork_6/TASK1/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] ComplitArray(int[] matr)
+{
+    for (int i = 0; i < matr.Length; i++)
+    {
+        System.Console.Write("Введите значение в массив M: ");
+        matr[i] = Convert.ToInt32(Console.ReadLine());
+    }    
+    return matr;
+}
+void PrintArray(int[] matr)
+{
+
+    for (int i = 0; i < matr.Length; i++)
+    {
+        System.Console.Write($"{matr[i]}, ");        
+    }
+    System.Console.WriteLine();
+}
+bool CheckNum(int n)
+{
+    if (n > 0) return true;
+    else return false;
+}
+
+int CounterArray(int[] matr)
+{
+    int count = 0;
+    for (int i = 0; i < matr.Length; i++)
+    {
+        if (CheckNum(matr[i])) count = count + 1;
+    }
+    return count;
+}
+
+System.Console.Write("Введите длину массива M: ");
+int leng = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[leng];
+ComplitArray(arr);
+int numPoz = CounterArray(arr);
+PrintArray(arr);
+System.Console.WriteLine($"Элементов больше 0 = {numPoz}");
+
