0c4ab5055079c6adc75edfcdebdcc747b78fda5a
sanich <sanich.vatt@mail.ru>
2023-07-08 04:27:23 +0300

    Home Work 5 - TASK1

---
+++ HomeWork_5/TASK1/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+// Задача 1: Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void PrintArray(int[] matr)
+{
+
+    for (int i = 0; i < matr.Length; i++)
+    {
+        System.Console.Write($"{matr[i]}, ");
+    }
+}
+
+bool CheckNum(int n)
+{
+    double i = Convert.ToInt32(n);
+    double div = i % 2;
+    if (div == 0) return true;
+    else return false;
+}
+
+int[] arr = new int[6];
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100, 999);
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (CheckNum(arr[i])) count = count + 1;
+}
+
+PrintArray(arr);
+System.Console.WriteLine();
+System.Console.WriteLine($"В массиве {count} четных значения");
