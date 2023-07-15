70f4b643cc677cc1f291aef9760b6533f9153ee4
sanich <sanich.vatt@mail.ru>
2023-07-15 11:54:11 +0300

    HomeWork_7 TASK1

---
+++ HomeWork_7/TASK1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write($"{message}");
+    string inputedString = Console.ReadLine();
+    int arg = Convert.ToInt32(inputedString);
+    return arg;
+}
+
+void PrintArray(double[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            System.Console.Write($"{matr[rows, columns]}, ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().NextDouble() + new Random().Next(-100,100);
+        }
+    }
+}
+
+int m = ReadInt("Введите колличество строк -> ");
+int n = ReadInt("Введите колличество столбцов -> ");
+double[,] matrix = new double[m, n];
+FillArray(matrix);
+PrintArray(matrix);
